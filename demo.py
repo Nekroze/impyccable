718c1c3dc7b25ceac42f3e136c0ae0664943ae23
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-14 23:36:14 +1000

    Extended the demo

--- demo.py
+++ demo.py
@@ -1,8 +1,16 @@
-from impyccable.generators import Boolean
-from impyccable.runner import Impyccable
+from impyccable.generators import Boolean, Integer
+from impyccable.runner import Impyccable, Runner
 
+print("===\nDecorator\n===")
 @Impyccable(Boolean())
 def test(val):
     print(val)
 
-test(20)
+test(5)
+
+print("===\nRunner\n===")
+def test2(val):
+    print(val)
+
+imptest = Runner(test2, Integer())
+imptest(5)
