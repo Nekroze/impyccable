1b9a8b91feb19bcd11a377f93f11363797c5313d
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-15 03:00:58 +1000

    Added String unittest

--- test/test_generators.py
+++ test/test_generators.py
@@ -1,7 +1,15 @@
 from impyccable.generators import *
 from impyccable.runners import Impyccable
+import string
 
 
-@Impyccable(Value(5))
+@Impyccable(Value(5), runs=100)
 def test_Value(val):
     assert val == 5
+
+
+@Impyccable(String(0, 10, string.lowercase), runs=100)
+def test_String(val):
+    assert 0 <= len(val) <= 10
+    for char in val:
+        assert char in string.lowercase
