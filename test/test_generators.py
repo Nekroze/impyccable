e3ff53f602d1859f76493f4fb7273a6581efc98c
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-15 11:17:18 +1000

    Moved tox and test asserts to unittest framework run with nose

--- test/test_generators.py
+++ test/test_generators.py
@@ -1,64 +1,60 @@
+from unittest import TestCase
 from impyccable.generators import *
 from impyccable.runners import Impyccable
-import unittest
 
 
 TEST_RUNS = 1000
 
 
-class GeneratorTests(unittest.TestCase):
+class GeneratorTests(TestCase):
 
     @Impyccable(Value(5), runs=TEST_RUNS)
     def test_Value(self, val):
-        assert val == 5
+        self.assertEqual(val, 5)
 
 
     @Impyccable(Choice([2,3,4]), runs=TEST_RUNS)
     def test_Float(self, val):
-        assert val in [2,3,4]
+        self.assertIn(val, [2,3,4])
 
 
     @Impyccable(String(0, 10, "abcdefghijklmnopqrstuvwxyz"), runs=TEST_RUNS)
     def test_String(self, val):
-        assert 0 <= len(val) <= 10
+        self.assertTrue(0 <= len(val) <= 10)
         for char in val:
-            assert char.islower()
+            self.assertTrue(char.islower())
 
 
     @Impyccable(Integer(0, 10), runs=TEST_RUNS)
     def test_Integer(self, val):
-        assert 0 <= val <= 10
+        self.assertTrue(0 <= val <= 10)
 
 
     @Impyccable(Float(0, 10), runs=TEST_RUNS)
     def test_Float(self, val):
-        assert 0 <= val <= 10
+        self.assertTrue(0 <= val <= 10)
 
 
     @Impyccable(Boolean(), runs=TEST_RUNS)
     def test_Boolean(self, val):
-        assert val is True or val is False
+        self.assertIn(val, (True, False))
 
 
     @Impyccable(List(Value(5), 5, 5), runs=TEST_RUNS)
     def test_List(self, val):
-        assert val == [5,5,5,5,5]
+        self.assertEqual(val, [5,5,5,5,5])
 
 
     @Impyccable(Tuple(Boolean(), Integer()), runs=TEST_RUNS)
     def test_Tuple(self, val):
-        assert len(val) == 2
-        assert isinstance(val[0], bool)
-        assert isinstance(val[1], int)
+        self.assertEqual(len(val), 2)
+        self.assertIsInstance(val[0], bool)
+        self.assertIsInstance(val[1], int)
 
 
     @Impyccable(Dictionary({"a": Boolean(), "z": String()}), runs=TEST_RUNS)
     def test_Dictionary(self, val):
-        assert "a" in val
-        assert isinstance(val["a"], bool)
-        assert "z" in val
-        assert isinstance(val["z"], str)
-
-
-if __name__ == '__main__':
-        unittest.main()
+        self.assertIn("a", val)
+        self.assertIsInstance(val["a"], bool)
+        self.assertIn("z", val)
+        self.assertIsInstance(val["z"], str)
