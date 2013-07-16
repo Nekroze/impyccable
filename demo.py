1f32c7f93ab40ae023c06c0aa599cd5b0eec4a73
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-16 20:40:40 +1000

    demo.py is now more useful

--- demo.py
+++ demo.py
@@ -1,15 +1,21 @@
-from impyccable.generators import Boolean, Integer
+from impyccable.generators import List, Integer
 from impyccable.runners import Impyccable, Runner
 
 print("===\nDecorator\n===")
-@Impyccable(bool)
+@Impyccable(List(bool))
 def test(val):
+    """
+    This function takes a list of booleans.
+    """
     print(val)
 
 test(runs=5)
 
 print("===\nRunner\n===")
 def test2(val):
+    """
+    The runner will supply test2 with randomly generated integers.
+    """
     print(val)
 
 imptest = Runner(test2, Integer())
